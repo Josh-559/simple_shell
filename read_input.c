6cd05d5ebcc00204d083cec97b2f70a46953a2c0
CYNTAX <samuelify225@gmail.com>
2023-04-26 20:43:51 +0100

    update on simple_shell

--- read_input.c
+++ read_input.c
@@ -7,45 +7,45 @@
  */
 int main(void)
 {
-        char *command;
-        char buffer[BUFFER_SIZE];
-        char *args[] = {buffer, NULL};
-        pid_t child_pid;
-        int status;
+	char *command;
+	char buffer[BUFFER_SIZE];
+	char *args[] = {buffer, NULL};
+	pid_t child_pid;
+	int status;
 
-        while (1)
-        {
-                printf("#cisfun$ ");
-                fflush(stdout);
+	while (1)
+	{
+		printf("#cisfun$ ");
+		fflush(stdout);
 
-                if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
-                        break;
+		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+			break;
 
-                buffer[strcspn(buffer, "\n")] = '\0';
+		buffer[strcspn(buffer, "\n")] = '\0';
 
-                child_pid = fork();
+		child_pid = fork();
 
-                if (child_pid == -1)
-                {
-                        fprintf(stderr, "%s: ", command);
-                        perror("");
-                        exit(1);
-                }
+		if (child_pid == -1)
+		{
+			fprintf(stderr, "%s: ", command);
+			perror("");
+			exit(1);
+		}
 
-                if (child_pid == 0)
-                {
-                        if (execve(args[0], args, NULL) == -1)
-                        {
-                                fprintf(stderr, "%s: ", command);
-                                perror("");
-                                exit(1);
-                        }
-                }
-                else
-                {
-                        waitpid(child_pid, &status, 0);
-                }
-        }
+		if (child_pid == 0)
+		{
+			if (execve(args[0], args, NULL) == -1)
+			{
+				fprintf(stderr, "%s: ", command);
+				perror("");
+				exit(1);
+			}
+		}
+		else
+		{
+			waitpid(child_pid, &status, 0);
+		}
+	}
 
-        return (0);
+	return (0);
 }
