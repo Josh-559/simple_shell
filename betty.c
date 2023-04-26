0cf3e07c1d9c63f3cdab73c9359b55b1a4328f73
Samuel Ifeanyi <samuelify225@gmail.com>
2023-04-26 06:15:22 -0700

    making betty happy

---
+++ betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    sum = num1 + num2;
+
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
